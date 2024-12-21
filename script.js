696a5f8a8784e841534d9af32d4c42be4b027e2a
karjix <128959844+KoaGarjix@users.noreply.github.com>
2024-12-21 19:07:44 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,12 +1,1 @@
-document.addEventListener("DOMContentLoaded", () => {
-  const audioPlayer = document.getElementById("audioPlayer");
-  const volumeSlider = document.getElementById("volumeSlider");
 
-  // Ensure audio starts playing automatically
-  audioPlayer.play();
-
-  // Update volume based on the slider's position
-  volumeSlider.addEventListener("input", () => {
-    audioPlayer.volume = volumeSlider.value;
-  });
-});
